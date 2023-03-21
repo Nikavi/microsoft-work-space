557379ca82f4f72e775e372be96150d197df7f4f
Veronika <104579294+Nikavi@users.noreply.github.com>
2023-03-21 14:32:22 +0100

    Added some test coded

--- hello.js
+++ hello.js
@@ -1,1 +1,27 @@
 console.log("Hello world!");
+
+const greet = "Hello Students, Welcome to the second week!";
+console.log(greet);
+
+console.log(greet.replace("second", "2nd"));
+console.log(greet);
+
+const students = "Students";
+const index = greet.indexOf(students);
+
+console.log("The index of the word students is " + index);
+
+//console.log(concat);
+
+console.log("This is\n" + "very long");
+console.log(`This story is very long`);
+
+console.log(`The length of the string is ${greet.length}`);
+
+const viewerCount = 50 * 2;
+console.log("The viewer count is " + viewerCount);
+console.log(`The viewer count is + ${viewerCount}`);
+console.log(`${viewerCount > 1 ? "great" : "not great"}`);
+
+const viewers = 10;
+console.log(viewers + 1);
