bc8e88ce9ea2d8309b3eae45335f895eb2d40fcc
Veronika <104579294+Nikavi@users.noreply.github.com>
2023-04-02 12:21:16 +0200

    new type of if..else statement

--- hello.js
+++ hello.js
@@ -66,3 +66,14 @@ let showLessons =
     ? console.log("Congrats")
     : console.log("Keep going");
 
+let currentMoney = 800;
+let laptopPrice = 1000;
+let laptopDiscountPrice = laptopPrice - laptopPrice * 0.2; //Laptop price at 20 percent off
+
+if (currentMoney >= laptopPrice || currentMoney >= laptopDiscountPrice) {
+  //Condition was true. Code in this block will run.
+  console.log("Getting a new laptop!");
+} else {
+  //Condition was true. Code in this block will run.
+  console.log("Can't afford a new laptop, yet!");
+}
